90a8d48a68ed2926a27c15e8a3162913fb213ebf
haozihong <haozihong@hotmail.com>
2021-03-10 01:27:24 -0500

    add hr mapper interface and sql

---
+++ sbhrm-db/src/main/java/com/zihong/sbhrm/mapper/HrMapper.java
@@ -0,0 +1,35 @@
+package com.zihong.sbhrm.mapper;
+
+import com.zihong.sbhrm.pojo.Hr;
+import com.zihong.sbhrm.pojo.Role;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface HrMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Hr record);
+
+    int insertSelective(Hr record);
+
+    Hr selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Hr record);
+
+    int updateByPrimaryKey(Hr record);
+
+    Hr loadUserByUsername(String username);
+
+    List<Role> getHrRolesById(Integer id);
+
+    List<Hr> getAllHrs(@Param("hrid") Integer hrid, @Param("keywords") String keywords);
+
+    List<Hr> getAllHrsExceptCurrentHr(Integer id);
+
+    Integer updatePasswd(@Param("hrid") Integer hrid, @Param("encodePass") String encodePass);
+
+    Integer updateAvatar(@Param("url") String url, @Param("id") Integer id);
+}
