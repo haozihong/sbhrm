e85cf58828fe8d2acf9bf3b277adbaad93d2b3ba
haozihong <haozihong@hotmail.com>
2021-03-10 18:42:17 -0500

    import Spring Security; edit Hr's pojo and service

--- sbhrm-db/src/main/java/com/zihong/sbhrm/pojo/Hr.java
+++ sbhrm-db/src/main/java/com/zihong/sbhrm/pojo/Hr.java
@@ -1,14 +1,24 @@
 package com.zihong.sbhrm.pojo;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
+/**
+ * @author Zihong Hao
+ * @github https://github.com/haozihong
+ * @date 2021-03-10 18:41
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-public class Hr {
+public class Hr implements UserDetails {
     private Integer id;
 
     private String name;
@@ -29,4 +39,33 @@ public class Hr {
 
     private String remark;
     private List<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
 }
--- sbhrm-db/src/main/java/com/zihong/sbhrm/service/HrService.java
+++ sbhrm-db/src/main/java/com/zihong/sbhrm/service/HrService.java
@@ -3,14 +3,31 @@ package com.zihong.sbhrm.service;
 import com.zihong.sbhrm.mapper.HrMapper;
 import com.zihong.sbhrm.pojo.Hr;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * @author Zihong Hao
+ * @github https://github.com/haozihong
+ * @date 2021-03-10 17:04
+ */
 @Service
-public class HrService {
+public class HrService implements UserDetailsService {
     @Autowired
     HrMapper hrMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Hr hr = hrMapper.loadUserByUsername(username);
+        if (hr == null) {
+            throw new UsernameNotFoundException("Username not found!");
+        }
+        return hr;
+    }
 
     public Hr getHrById(Integer id) {
         return hrMapper.selectByPrimaryKey(id);
