34f406554e0537adadcef6961466108060791604
haozihong <haozihong@hotmail.com>
2021-03-11 17:15:45 -0500

    make basic home page

--- sbhrm-vue/src/views/Home.vue
+++ sbhrm-vue/src/views/Home.vue
@@ -1,18 +1,125 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1', '2']">
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-user"></i>User Info</template>
+            <el-menu-item index="1-1">
+              <router-link to="/hr/basic">Basic Info</router-link>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-menu"></i>HR Management</template>
+            <el-menu-item index="2-1">
+              <router-link to="/hr/list">All HR</router-link>
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"><i class="el-icon-setting"></i>Navi III</template>
+            <el-menu-item-group>
+              <template slot="title">Group 1</template>
+              <el-menu-item index="3-1">Option 1</el-menu-item>
+              <el-menu-item index="3-2">Option 2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group 2">
+              <el-menu-item index="3-3">Option 3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="3-4">
+              <template slot="title">Option 4</template>
+              <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="4">
+            <router-link to="/about"><i class="el-icon-info"></i>About</router-link>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>查看</el-dropdown-item>
+              <el-dropdown-item>新增</el-dropdown-item>
+              <el-dropdown-item>删除</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>王小虎</span>
+        </el-header>
+
+        <el-main>
+          <el-breadcrumb separator-class="el-icon-arrow-right" v-if="this.$router.currentRoute.path!='/'">
+            <el-breadcrumb-item :to="{ path: '/' }">Home</el-breadcrumb-item>
+            <el-breadcrumb-item>{{this.$router.currentRoute.name}}</el-breadcrumb-item>
+          </el-breadcrumb>
+          <div class="homeWelcome" v-if="this.$router.currentRoute.path=='/'">
+            Welcome to Quick HR!
+          </div>
+          <router-view class="homeRouterView"/>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style>
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+
+.homeRouterView {
+  margin-top: 10px;
+}
+
+.homeWelcome {
+  text-align: center;
+  font-size: 30px;
+  color: #409eff;
+  padding-top: 50px;
+}
+
+.homeHeader {
+  background-color: #409eff;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 15px;
+  box-sizing: border-box;
+}
+
+.homeHeader .title {
+  font-size: 30px;
+  color: #ffffff
+}
+
+.homeHeader .userInfo {
+  cursor: pointer;
+}
+
+.el-dropdown-link img {
+  width: 48px;
+  height: 48px;
+  border-radius: 24px;
+  margin-left: 8px;
+}
+
+.el-dropdown-link {
+  display: flex;
+  align-items: center;
+}
+
+</style>
