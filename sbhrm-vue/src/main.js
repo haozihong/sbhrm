fb530f2fadee774252a90c725f8df54c9fbd91bd
haozihong <haozihong@hotmail.com>
2021-03-11 17:50:29 -0500

    import axios and vue-axios

--- sbhrm-vue/src/main.js
+++ sbhrm-vue/src/main.js
@@ -4,10 +4,14 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
