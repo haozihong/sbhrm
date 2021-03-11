abf2608a7965f06ed11cff377d655dd15129e7dd
haozihong <haozihong@hotmail.com>
2021-03-11 13:54:28 -0500

    import and test element-ui

--- sbhrm-vue/src/App.vue
+++ sbhrm-vue/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     <h1>Hello world</h1>
+    <div>
+      <el-button @click="startHacking">Start</el-button>
+    </div>
   </div>
 </template>
 
@@ -8,6 +11,16 @@
 export default {
   name: 'App',
   components: {
+  },
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It works!',
+        type: 'success',
+        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+        duration: 5000
+      })
+    }
   }
 }
 </script>
