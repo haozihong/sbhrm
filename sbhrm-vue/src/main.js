024ddf248af8e20b481cca17fc9114ff149b3cd5
haozihong <haozihong@hotmail.com>
2021-03-12 12:12:30 -0500

    import Vuex

--- sbhrm-vue/src/main.js
+++ sbhrm-vue/src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
 import router from './router'
+
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
 
@@ -13,7 +17,19 @@ Vue.use(ElementUI);
 
 Vue.use(VueAxios, axios)
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    currentUser: null
+  },
+  mutations: {
+
+  }
+})
+
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
